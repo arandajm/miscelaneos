0068d1508ad5fa308e6dab8a33b680e049319f1f
Jesus Aranda <aranda.jm@icloud.com>
2020-04-16 17:02:07 -0300

    Access to the parent params from child route

--- src/app/components/usuario/usuario-nuevo.component.ts
+++ src/app/components/usuario/usuario-nuevo.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-usuario-nuevo',
   templateUrl: './usuario-nuevo.component.html',
 })
 export class UsuarioNuevoComponent implements OnInit {
-  constructor() {}
+  constructor(private router: ActivatedRoute) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    // Access to the parent params, in this case id
+    this.router.parent.params.subscribe((params) => {
+      console.log('Ruta hija!!');
+      console.log(params);
+    });
+  }
 }
--- src/app/components/usuario/usuario.component.ts
+++ src/app/components/usuario/usuario.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-usuario',
   templateUrl: './usuario.component.html',
 })
 export class UsuarioComponent implements OnInit {
-  constructor() {}
+  constructor(private router: ActivatedRoute) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.router.params.subscribe((params) => {
+      console.log('Ruta Padre!!');
+      console.log(params);
+    });
+  }
 }
